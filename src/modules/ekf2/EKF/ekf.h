fb762868f9729d1590073a2c11bcecd7802b87f2
Daniel Agar <daniel@agar.ca>
2022-02-08 11:01:36 -0500

    ekf2: add simple zero velocity update when vehicle at rest

--- src/modules/ekf2/EKF/ekf.h
+++ src/modules/ekf2/EKF/ekf.h
@@ -384,6 +384,7 @@ private:
 	uint64_t _time_last_arsp_fuse{0};	///< time the last fusion of airspeed measurements were performed (uSec)
 	uint64_t _time_last_beta_fuse{0};	///< time the last fusion of synthetic sideslip measurements were performed (uSec)
 	uint64_t _time_last_fake_pos_fuse{0};	///< last time we faked position measurements to constrain tilt errors during operation without external aiding (uSec)
+	uint64_t _time_last_zero_velocity_fuse{0}; ///< last time of zero velocity update (uSec)
 	uint64_t _time_last_gps_yaw_fuse{0};	///< time the last fusion of GPS yaw measurements were performed (uSec)
 	uint64_t _time_last_gps_yaw_data{0};	///< time the last GPS yaw measurement was available (uSec)
 	uint8_t _nb_gps_yaw_reset_available{0}; ///< remaining number of resets allowed before switching to another aiding source
@@ -873,6 +874,8 @@ private:
 
 	// control fusion of fake position observations to constrain drift
 	void controlFakePosFusion();
+
+	void controlZeroVelocityUpdate();
 
 	// control fusion of auxiliary velocity observations
 	void controlAuxVelFusion();
